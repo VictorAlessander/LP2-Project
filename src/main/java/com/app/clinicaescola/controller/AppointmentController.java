2997c3aaaef7c1149cae24bfca75c712859c7a7a
Victor Alessander <alessander@protonmail.com>
2017-11-07 16:52:09 -0300

    Implemented a exception handler and some little changes

--- src/main/java/com/app/clinicaescola/controller/AppointmentController.java
+++ src/main/java/com/app/clinicaescola/controller/AppointmentController.java
@@ -8,8 +8,13 @@ package com.app.clinicaescola.controller;
 import com.app.clinicaescola.entity.Appointment;
 import com.app.clinicaescola.service.AppointmentService;
 import com.app.clinicaescola.service.EmployeeService;
+import java.io.IOException;
 import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,31 +35,49 @@ public class AppointmentController {
     @Autowired
     EmployeeService employee;
     
-    @RequestMapping(value = "/appointments/{firstName}", method = RequestMethod.GET)
-    public List<Appointment> list(@PathVariable("firstName") String employeeFirstName) {
+    @RequestMapping(value = "/appointments/{firstName}",
+            method = RequestMethod.GET)
+    public List<Appointment> list(
+            @PathVariable("firstName") String employeeFirstName) {
+
         return this.appointment.listAllAppointments();
     }
     
-    @RequestMapping(value = "/appointments/{firstName}", method = RequestMethod.POST)
+    @RequestMapping(value = "/appointments/{firstName}",
+            method = RequestMethod.POST)
     public List<Appointment> create(
             @RequestBody Appointment newAppointment,
             @PathVariable("firstName") String employeeFirstName) {
         
-        return this.appointment.newAppointment(newAppointment, employeeFirstName);
+        return this.appointment.newAppointment(
+                newAppointment, employeeFirstName);
     }
     
-    @RequestMapping(value = "/appointments/{firstName}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/appointments/{firstName}",
+            method = RequestMethod.PUT)
     public List<Appointment> update(
             @RequestBody Appointment appointment,
             @PathVariable("firstName") String employeeFirstName) {
         
-        return this.appointment.newAppointment(appointment, employeeFirstName);
+        return this.appointment.newAppointment(
+                appointment, employeeFirstName);
     }
     
-    @RequestMapping(value = "/appointments/{id}/{firstName}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/appointments/{id}/{firstName}",
+            method = RequestMethod.DELETE)
     public void delete(
             @PathVariable("id") String id,
             @PathVariable("firstName") String employeeFirstName) {
-        this.appointment.removeAppointment(employeeFirstName, employeeFirstName);
+
+        this.appointment.removeAppointment(
+                employeeFirstName, employeeFirstName);
+    }
+    
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public void handleBadRequest(
+            HttpServletResponse response) throws IOException {
+        response.sendError(
+                HttpStatus.BAD_REQUEST.value(),
+                "Insert a date with format 'dd-MM-yyyy'");
     }
 }
