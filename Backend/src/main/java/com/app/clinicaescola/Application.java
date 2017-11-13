725c4f8aea26c44db4a2eb76577532917ffa86d0
Victor Alessander <alessander@protonmail.com>
2017-11-13 01:13:23 -0300

    Added CORS configuration

--- Backend/src/main/java/com/app/clinicaescola/Application.java
+++ Backend/src/main/java/com/app/clinicaescola/Application.java
@@ -7,6 +7,10 @@ package com.app.clinicaescola;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  *
@@ -18,6 +22,19 @@ public class Application {
     
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = 
+                new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
     
 }
